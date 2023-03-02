d423d7fe5ab66ee5e2bb5adf78c6b6119c561847
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2023-03-02 03:06:30 -0600

    added cta band for home page

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "../styles.css";
 
 function CTA() {
   return (
     <Container fluid={true} className="custom-cta">
-      <Row className="custom-container-row">
+      <Row>
         <Col className='text-center band-header'>
           <p>Unleash Potential</p>
         </Col>
         </Row>
-        <Row className="custom-container-row">
-        <Col className='text-center'>
-          <p>© 2023 Wallen-Douglas Digital</p>
+        <Row>
+        <Col className='text-center body-copy-bold'>
+          <p>Unlock the full potential of your business with Wallen-Douglas Digital - get in touch to see how our expertise and tailored solutions can drive your success.</p>
         </Col>
       </Row>
-      <Row className="custom-container-row">
+      <Row>
         <Col className='text-center'>
-          <p>© 2023 Wallen-Douglas Digital</p>
+        <Button className='custom-button mx-auto'>Get In Touch</Button>
         </Col>
       </Row>
     </Container>
