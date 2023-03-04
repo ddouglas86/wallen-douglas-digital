a366b0d9f7dc738c07de1b93460bb9aed50c9192
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2023-03-03 23:12:05 -0600

    separated home hero into its own js file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,16 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import CTABar from "../components/CTABar.js";
 import ServiceBand from "../components/ServiceBand.js";
 import Testimonials from "../components/Testimonials.js";
-import hero from "../home_hero.jpg";
+import HomeHero from "../components/HomeHero.js";
 import "../styles.css";
+
 
 function Home() {
     return (
         <Row>
-            <Container fluid={true} className="home-hero" style={{ marginTop: '11vh' }}>
-                <Col md={4} className='custom-col-pad-left'>
-                    <Row className='home-hero-main-text'>
-                        <p>Unlocking Business Growth through Digital Innovation</p>
-                    </Row>
-                    <Row className='body-copy-bold'>
-                        <p>Experience exponential business growth through our innovative and customized digital services, tailored to your unique needs.</p>
-                    </Row>
-                    <Row>
-                        <Button className='custom-button mx-auto'>Get In Touch</Button>
-                    </Row>
-                </Col>
-                <Col md={4} className='custom-col-pad-center'>
-                    <Row>
-                        <img src={hero} alt='Wallen-Douglas Digital logo' className='hero-image' />
-                    </Row>
-                </Col>
-                <Col md={4} className='custom-margin'>
-                    <Row className='body-copy-bold'>
-                        <p>Implement data-driven strategies to improve the user experience and increase engagement, with a focus on customer acquisition and retention.</p>
-                        <p>Leverage the latest technologies and trends to develop innovative and effective digital solutions tailored to your unique business needs.</p>
-                        <p>Leverage data and analytics to provide valuable insights, identify opportunities, and inform strategic decision-making for high growth companies</p>
-                    </Row>
-                </Col>
-            </Container>
+            <HomeHero />
             <CTABar />
             <ServiceBand />
             <Testimonials />
