8f5fd3b89e92beee3e9287c8fa660bcc6c4fc832
DDouglas <53360057+ddouglas86@users.noreply.github.com>
2023-03-10 10:02:32 -0600

    added cta band to services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Row } from "react-bootstrap";
 import ServiceHero from "../components/HeroSections/ServiceHero.js";
+import DarkCTA from "../components/CTAs/DarkCTA.js";
 import "../styles.css";
 
 
 function Services() {
     return (
         <Row>
-            <ServiceHero />        
+            <ServiceHero /> 
+            <DarkCTA />
         </Row>
     );
 }
